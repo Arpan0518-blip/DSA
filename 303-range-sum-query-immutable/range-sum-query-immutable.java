33413ec1fe8342ec4f91eb394e4024d4cc692da3
Arpan Upadhyay <upadhyayjii8090@gmail.com>
2026-09-15 02:04:36 +0530

    Time: 7 ms (99.95%) | Memory: 47.8 MB (43.45%) - LeetSync

--- 303-range-sum-query-immutable/range-sum-query-immutable.java
+++ 303-range-sum-query-immutable/range-sum-query-immutable.java
@@ -1,25 +1,81 @@
-class NumArray {
-    int [] pref;
+// public class main{ 
+ 
+class NumArray { 
+    int [] pref; 
+ 
+    public NumArray(int[] nums) { 
+        int n = nums.length; 
+        pref = new int[n]; 
+         
+        pref[0] = nums[0]; 
+        for(int i = 1; i < n ; i++){ 
+            pref[i] = pref[i-1] + nums[i]; 
+        } 
+    } 
+     
+    public int sumRange(int left, int right) { 
+ 
+        if (left == 0) { 
+            return pref[right]; 
+        } 
+ 
+        return pref[right] - pref[left - 1]; 
+         
+    } 
+ 
+} 
 
-    public NumArray(int[] nums) {
-        int n = nums.length;
-        pref = new int[n];
-        
-        pref[0] = nums[0];
-        for(int i = 1; i < n ; i++){
-            pref[i] = pref[i-1] + nums[i];
-        }
-           
-    }
-    
-    public int sumRange(int left, int right) {
 
-        if (left == 0) {
-            return pref[right];
-        }
+// public static void main(String[]args){ 
+//     int[] nums = {-2, 0, 3, -5, 2, -1}; 
+ 
+//     NumArray obj = new NumArray(nums); 
+ 
+//     // System.out.println(obj.sumRange(0, 2)); 
+//     // System.out.println(obj.sumRange(2, 5)); 
+//     // System.out.println(obj.sumRange(0, 5)); 
+// } 
 
-        return pref[right] - pref[left - 1];
-        
-    }
-}
 
+
+
+
+
+
+
+// ------------------------------------------------------------
+
+// public class Main {
+
+//     static int[] pref;
+
+//     static void prefixSum(int[] nums, int left, int right) {
+
+//         int n = nums.length;
+
+//         pref = new int[n];
+
+//         pref[0] = nums[0];
+
+//         for (int i = 1; i < n; i++) {
+//             pref[i] = pref[i - 1] + nums[i];
+//         }
+
+
+//         if (left == 0) {
+//             System.out.println(pref[right]);
+//         } 
+//         else {
+//             System.out.println(pref[right] - pref[left - 1]);
+//         }
+//     }
+
+//     public static void main(String[] args) {
+
+//         int[] nums = {-2, 0, 3, -5, 2, -1};
+
+//         prefixSum(nums, 0, 2);
+//         prefixSum(nums, 2, 5);
+//         prefixSum(nums, 0, 5);
+//     }
+// }
