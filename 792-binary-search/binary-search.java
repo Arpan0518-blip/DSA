3d4ef680529d29b1e97bccefcb7528e3918f8004
Arpan Upadhyay <upadhyayjii8090@gmail.com>
2026-09-15 12:13:29 +0530

    Time: 0 ms (100.00%) | Memory: 48 MB (94.30%) - LeetSync

---
+++ 792-binary-search/binary-search.java
@@ -0,0 +1,30 @@
+// class Solution {
+//     public int search(int[] nums, int target) {
+//         for(int i = 0; i < nums.length-1; i++){
+//             if(nums[i]== target){
+//                 return i;
+//             }
+//         }
+//         return -1;
+        
+//     }
+// }
+
+
+
+// -----------------------------------------------
+
+class Solution {
+    public int search(int[] nums, int target) {
+        int st = 0, end = nums.length-1;
+
+        while(st<=end){
+            int mid = st+(end-st)/2;
+            if(target == nums[mid]) return mid;
+            else if(target > nums[mid])  st = mid+1;
+            else end = mid-1;
+        }
+        return -1;
+        
+    }
+}
